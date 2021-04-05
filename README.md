6e5608a073cf0d1ff11b54383b64cde373240cad
FlorentinaSimlinger <60526092+FlorentinaSimlinger@users.noreply.github.com>
2021-04-05 10:35:40 +0100

    add basic input stream reader

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ## To run:
 
 
-## Soruces used:
+## Sources used:
+- for reading input stream and piping: https://stackoverflow.com/questions/5724646/how-to-pipe-input-to-java-program-with-bash?noredirect=1&lq=1
+
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,22 @@
 package Main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            InputStreamReader isReader = new InputStreamReader(System.in);
+            BufferedReader bufReader = new BufferedReader(isReader);
+            String inputString = "";
+            while (inputString != null) {
+                inputString = bufReader.readLine();
+                System.out.println(inputString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
