10c7b36ac2c82b480d5b61d6a5b6280cf376bf98
FlorentinaSimlinger <60526092+FlorentinaSimlinger@users.noreply.github.com>
2021-04-05 11:24:30 +0100

    add gson jar file and running instructions

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # MongoDB-coding-challenge
 
 ## To run:
-
+- install Java 11
+- navigate into the src folder of the project
+- javac -cp .:../gson-2.8.6.jar Main/Main.java
+- cat path-to-json-file.json | java -cp .:../gson-2.8.6.jar Main/Main
+where path-to-json-file.json is for instance ../basic.json for the test file I added
+for testing purposes
 
 ## Sources used:
 - for reading input stream and piping: https://stackoverflow.com/questions/5724646/how-to-pipe-input-to-java-program-with-bash?noredirect=1&lq=1
-
+- for debugging compiling and running Java program from command line: https://stackoverflow.com/questions/18093928/what-does-could-not-find-or-load-main-class-mean
+- for adding external libraries without Maven: https://stackoverflow.com/questions/10656428/include-gson-in-my-java-program
+- for adding gson to classpath when compiling and running: https://stackoverflow.com/questions/10656428/include-gson-in-my-java-program
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,9 @@
 package Main;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,13 +14,30 @@ public class Main {
         try {
             InputStreamReader isReader = new InputStreamReader(System.in);
             BufferedReader bufReader = new BufferedReader(isReader);
-            String inputString = "";
-            while (inputString != null) {
-                inputString = bufReader.readLine();
-                System.out.println(inputString);
+            String inputStr = "";
+            StringBuilder sb = new StringBuilder();
+            while (inputStr != null) {
+                inputStr = bufReader.readLine();
+                if (inputStr != null) {
+                    sb.append(inputStr);
+                    System.out.println(inputStr);
+                }
             }
+            System.out.println("reading done");
+            String jsonString = sb.toString();
+            System.out.println(jsonString);
+            convertToJsonObj(jsonString);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    // EFFECTS: converts input string to json object
+    private static void convertToJsonObj(String jsonString) {
+        JsonObject obj = JsonParser.parseString(jsonString).getAsJsonObject();
+        System.out.println(obj);
+
+    }
+
+
 }
